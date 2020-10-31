5d75cbc7c5bff845d6c49f583833efdfe86d1239
Marco Mendes <marco.mendes@sroque.pt>
2020-10-31 20:46:34 +0000

    alteracoes utilizadores

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -16,7 +16,7 @@
       <ul class="sidebar-menu" data-widget="tree">
 
          <li class="active"><a href="{{route('home')}}"><i class="fas fa-home"></i> <span>Home</span></a></li>
-
+        @if(Auth::user()->isinrule(['supermaster']))
          <li class="treeview">
             <a href="#"><i class="fas fa-user"></i> <span>Utilizadores</span>
               <span class="pull-right-container">
@@ -25,11 +25,12 @@
             </a>
             <ul class="treeview-menu">
               <li><a href="{{route('user')}}"><i class="fas fa-user"></i><span> Utilizadores</span></a></li>
-               @if(Auth::user()->isinrule(['supermaster']))
+            
                   <li><a href="{{route('role')}}"><i class="fas fa-gavel"></i> <span>Regras</span></a></li> 
-               @endif
+             
             </ul>
           </li>
+         @endif
          @if(Auth::user()->isinrule(['supermaster']))
          <li class="treeview">
             <a href="#"><i class="fas fa-users"></i> <span> Grupos</span>
