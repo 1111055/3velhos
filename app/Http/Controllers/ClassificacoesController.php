5b9471f21795d1cf4e380c1c878baf2e9ddbeef7
Marco Mendes <marcomendes0202@hotmail.com>
2021-04-01 08:31:24 +0100

    alteraçoes
    alteracoes

--- app/Http/Controllers/ClassificacoesController.php
+++ app/Http/Controllers/ClassificacoesController.php
@@ -7,6 +7,7 @@ use App\Classificacao;
 use App\Usergrupo;
 use App\Jogo;
 use App\Aposta;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -44,8 +45,8 @@ class ClassificacoesController extends Controller
        $mes = $now->month;
 
 
-
-        if($idgrupo == -1){
+       $users_tmp = User::where('activo', '=','1')->pluck('ID')->all();
+      /*  if($idgrupo == -1){
             $todos = true;
         }elseif($idgrupo == 0){
            $grupo  = Usergrupo::where('user_id', '=',$user->id)->first();
@@ -56,27 +57,29 @@ class ClassificacoesController extends Controller
        if($idgrupo != -1){
            $grupoallusers = Usergrupo::where('grupo_id', '=',$grupo->grupo_id)->pluck('user_id')->toArray();
         }
-      //  dd($grupoallusers);
 
+    
+       
+        $teste_tmp = array_intersect(  $grupoallusers,  $users_tmp);
+    
         if($todos == false){
 
            
-            
-           
-
              $apostastmp = Aposta::whereMonth('created_at', '=', $mes) ;
 
         //     dd($apostastmp);  
 
-            $class = Classificacao::whereIn('user_id', $grupoallusers)-> 
+            $class = Classificacao::whereIn('user_id', $teste_tmp)-> 
                      orderBy('pontos','desc')->get();
         }else{
 
 
-             $class = Classificacao:: 
-                 orderBy('pontos','desc')->get();
+               $class = Classificacao::whereIn('user_id', $users_tmp)-> 
+                     orderBy('pontos','desc')->get();
 
-        }
+        }*/
+         $class = Classificacao::whereIn('user_id', $users_tmp)-> 
+                     orderBy('pontos','desc')->get();
        //  dd($class);
 
 
@@ -92,13 +95,18 @@ class ClassificacoesController extends Controller
             $teste2 = DB::table('resutladosestatisticas')->where('result', '=', 1)->whereMonth('created_at', '=', $mes)->where('user_id','=', $value->utilizador[0]->id)->count();
          
 
-            //dd($teste2);
-            if($teste == 0 ) {$teste = 1;}
-
 
            $value['nome'] = $value->utilizador[0]->name;
-            $perc = ($teste2 * 100) / $teste;
+
+            if($teste > 0){
+                $perc = ($teste2 * 100) / $teste;
+            }
+            else{
+                 $perc = 0;
+            }
+
             $value['percentagens'] = $teste2."/".$teste." (".(int)$perc."%)";
+
 
         }         
 
